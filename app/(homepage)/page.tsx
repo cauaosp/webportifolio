056b2462bb283c6c65c1fe931b9cb298c3a55430
cauaosp <cauaosp20@gmail.com>
2025-02-03 15:01:29 -0300

    refactor: adicionado virgula depois de ola no texto do homepage

--- app/(homepage)/page.tsx
+++ app/(homepage)/page.tsx
@@ -9,7 +9,7 @@ export default function HomePage() {
       <div className="grid grid-rows-3 grid-cols-3 p-5 2xl:pt-10 gap-4 gap-y-0 md:gap-y-20 2xl:gap-y-0 overflow-hidden 2xl:mt-8 2xl:mb-0">
         <Profile />
         <div className="col-span-2 text-white text-justify 2xl:text-lg">
-          Olá sou <span className="text-emerald-400">Cauã OSP</span>, desenvolvedor nascido e criado em Fortaleza, CE, tenho{" "}
+          Olá, sou <span className="text-emerald-400">Cauã OSP</span>, desenvolvedor nascido e criado em Fortaleza, CE, tenho{" "}
           <AgeCounter />
           ... Atualmente, meu foco é o desenvolvimento Full Stack,
           busco dominar ferramentas e tecnologias para criar soluções completas
